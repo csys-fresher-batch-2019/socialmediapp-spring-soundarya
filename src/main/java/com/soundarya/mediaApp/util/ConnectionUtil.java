2f551e6600b18c7e4284fcb1d564f2015cc2538b
Soundarya P <soundmithu@gmail.com>
2020-03-12 11:02:45 +0530

    updated

--- src/main/java/com/soundarya/mediaApp/util/ConnectionUtil.java
+++ src/main/java/com/soundarya/mediaApp/util/ConnectionUtil.java
@@ -2,13 +2,24 @@ package com.soundarya.mediaApp.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.soundarya.mediaApp.exception.DBException;
+import com.soundarya.mediaApp.exception.ErrorConstant;
 
 public class ConnectionUtil {
-	public static Connection conMethod() throws Exception {
+	public static Connection conMethod() throws ClassNotFoundException, DBException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle");
+		Connection con;
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle");
+			// con=DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE","soundarya","soundarya");
 
-		// con=DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE","soundarya","soundarya");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new DBException(ErrorConstant.INVALID_CON);
+		}
 
 		System.out.println("connected");
 		return con;
